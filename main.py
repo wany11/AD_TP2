33c18d58855b69a2c9d9e8178d0c67785e714d33
thomas <thomaspwthomaspw@gmail.com>
2024-12-09 14:47:56 +0100

    comparison between maths science and drawing

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ data = pd.read_csv("notes.csv", sep=';')
 X = data.set_index(data.columns[0]).transpose()
 
 nomi = list(X.index)
-nomv = list(X.columns)
 
-french_grades = X['fran']
-latin_grades = X['lati']
+math_scores = X['math']
+science_scores = X['scie']
+drawing_scores = X['d-m ']
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
-ax1.hist(french_grades, edgecolor='black', alpha=0.7)
-ax1.set_title('Distribution des notes de Français')
-ax1.set_xlabel('Notes')
-ax1.set_ylabel('Frequency')
+for i in range(len(nomi)):
+    ax1.scatter(math_scores[i], science_scores[i])
+    ax1.text(math_scores[i], science_scores[i], nomi[i], fontsize=9, ha='right')
+ax1.set_title('Mathematique vs Sciences')
+ax1.set_xlabel('Mathematique')
+ax1.set_ylabel('Sciences')
 
-ax2.hist(latin_grades, edgecolor='black', alpha=0.7)
-ax2.set_title('Distribution des notes de Latin')
-ax2.set_xlabel('Notes')
-ax2.set_ylabel('Frequency')
+for i in range(len(nomi)):
+    ax2.scatter(math_scores[i], drawing_scores[i])
+    ax2.text(math_scores[i], drawing_scores[i], nomi[i], fontsize=9, ha='right')
+ax2.set_title('Mathematique vs Dessin')
+ax2.set_xlabel('Mathematique')
+ax2.set_ylabel('Dessin')
 
 plt.tight_layout()
 
